f8762ed66fa7f15fc4dceea2cfc7c837e163dc24
Paweł Wilkosz <pwilkosz5@gmail.com>
2018-10-21 21:42:20 +0200

    konflikty, wojna itp

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Witajcie w mojej kuchni");
+        System.out.println("Pozdrawiam całą rodzinę");
     }
 }
