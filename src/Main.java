c97f97cb37ba3928cb897d64f02d7f434fce6683
LazaniaPL <44317789+LazaniaPL@users.noreply.github.com>
2018-10-22 09:21:56 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Pozdrawiam całą rodzinę");
+        System.out.println("Pozdrawiam rodzinę");
     }
 }
